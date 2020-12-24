7acd53ee8ea0712d50fb6f40f916173bd43bdf2c
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-12-24 08:31:12 +0530

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-root',
+  //selector: '[app-root]',
+  selector: '.app-root',
+  // template:"<h1>Hello World</h1>"
+  // template:`<h1>Hello World</h1>
+  //           <h2>Multi line Html example</h2>
+  // `
+  templateUrl:"./app.component.html",
+  // styles:[
+  //   `div{background-color:yellow}`
+  // ]
+  styleUrls:[
+    "./app.component.css"
+  ]
 })
 export class AppComponent {
-  title = 'December2020Batch2';
+  
 }
