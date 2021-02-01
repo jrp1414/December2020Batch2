e744c0dc971dfd42255312c3409d9b5dfaae4da0
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2021-02-01 07:37:29 +0530

    Reactive form Example 2

--- src/app/students/student-edit/student-edit.component.ts
+++ src/app/students/student-edit/student-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Student } from '../services/student.data';
 import { StudentService } from '../services/student.service';
@@ -11,25 +11,45 @@ import { StudentService } from '../services/student.service';
 })
 export class StudentEditComponent implements OnInit {
 
-  studentEditForm:FormGroup;
-  // firstName:FormControl = new FormControl();
-  // lastName:FormControl = new FormControl();
-  student:Student;
-  constructor(private route:ActivatedRoute,private ss:StudentService) { }
+  studentEditForm: FormGroup;
+  student: Student;
+  constructor(private route: ActivatedRoute, private ss: StudentService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((parms)=>{
+    this.route.params.subscribe((parms) => {
       this.student = this.ss.getStudent(parms.id);
-      this.studentEditForm = new FormGroup({
-        FirstName: new FormControl(this.student.FirstName),
-        LastName: new FormControl("acsdsv") 
-      }); 
+      this.studentEditForm = this.fb.group({
+        FirstName: this.fb.control(this.student.FirstName),
+        LastName: this.fb.control(this.student.LastName),
+        MobileNo: this.fb.control(this.student.MobileNo),
+        EmailId: this.fb.control(this.student.EmailId),
+        NotificationType: this.fb.control(this.student.NotificationType),
+        Address: this.fb.group({
+          AddLine1: this.fb.control(this.student.Address.AddLine1),
+          AddLine2: this.fb.control(this.student.Address.AddLine2),
+          AddLine3: this.fb.control(this.student.Address.AddLine3),
+          City: this.fb.control(this.student.Address.City),
+          State: this.fb.control(this.student.Address.State)
+        }),
+        TermsAndConditions: this.fb.control(true),
+        Skills: this.fb.control("Angular"),
+        Comments: this.fb.control(""),
+        Hobbies: this.fb.array([
+          this.fb.control("")
+        ])
+      })
     });
-    
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.studentEditForm.value);
+  }
+
+  AddHobby() {
+    let hobbies = <FormArray>this.studentEditForm.get("Hobbies");
+    if (hobbies.controls.length<10) {
+      (hobbies).push(this.fb.control(""));  
+    }    
   }
 
 }
